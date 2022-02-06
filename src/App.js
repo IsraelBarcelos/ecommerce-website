4448cf7c3039589fe3fe7259c30ce01c27fda33b
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-02-05 21:16:57 -0300

    components on root

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Categories from "./components/Categories";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Recommend from "./components/Recommend";
+import Choose from "./components/Choose";
+import Footer from "./components/Footer";
+import Promo from "./components/Promo";
+import Products from "./components/Products";
+import "./scss/index.scss";
 
 export default function App() {
-  return <div>hello</div>;
+  return <div className="app">
+    <ScrollToTop />
+    <NavBar />
+    <Home />
+    <Services />
+    <Categories />
+    <Recommend />
+    <Choose />
+    <Products />
+    <Promo />
+    <Footer />
+  </div>;
 }
